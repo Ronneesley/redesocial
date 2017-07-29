878108fba629ac8d3409a117353d9e2f69a70f8c
Ianka Talita <30481954+iankatalitaa@users.noreply.github.com>
2017-07-28 22:35:03 -0300

    Comentário

---
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dto/comentario.java
@@ -0,0 +1,122 @@
+
+package br.com.redesocial.modelo.dto;
+
+import java.sql.Date;
+
+public class comentario {
+    
+    private Integer id;
+    
+    private String descricao;
+    
+    private Integer curtidas;
+    
+    private Date data;
+    
+    private Postagem postagem;
+    
+    private Resposta resposta;
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getCurtidas() {
+        return curtidas;
+    }
+
+    public void setCurtidas(String curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public String getData() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getData_cadastro() {
+        return data_cadastro;
+    }
+
+    public void setData_cadastro(String data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    @Override
+    public String toString() {
+        return getNome();
+    }
+}
+
+}
+
+
+
+
+
+    
