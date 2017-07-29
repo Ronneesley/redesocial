893b0fd204691e3ba41363da205073b05fec286b
LuckasAzevedo <luckaskinha@gmail.com>
2017-07-29 16:54:48 -0300

    Classe Estado e Pais
    Codigo das classe estado e pais

---
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dto/estado.java
@@ -0,0 +1,44 @@
+package br.com.redesocial.modelo.dto;
+ /**
+
+  * @author Lucas, Luciano, Adalberto
+  * @since 29/07/2017
+  */
+
+public class Estado {
+    private int id;
+	private String nome;
+	private Pais pais;
+
+    public Estado(int id, String nome,int pais) {
+       this.id = id;
+       this.nome = nome;
+	   this.pais = pais;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+	
+	public int getPais() {
+        return pais;
+    }
+
+    public void setPais(int pais) {
+        this.pais = pais;
+    }
+    
+}
