08d3d1045478e8a495f605ae22eee8f0607cc5d4
Daniel Moreira <qualquercoisa6568@hotmail.com>
2017-07-29 14:12:56 -0300

    Correção 1 DER
    adição de layers e mudança na classe cidade

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dto/Cidade.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dto/Cidade.java
@@ -9,7 +9,7 @@ public class Cidade {
     private Estado estado;
     private String nome;
 
-    public Cidade(int id, int estado, String nome) {
+    public Cidade(int id, Estado estado, String nome) {
         this.id = id;
         this.estado = estado;
         this.nome = nome;
