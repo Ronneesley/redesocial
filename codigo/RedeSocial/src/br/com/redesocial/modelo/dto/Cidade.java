0b05b865b9dce497fa9ace89066395fadb522004
Paulo Henrique <30481980+PHRA-IFGOIANO@users.noreply.github.com>
2017-07-28 22:26:22 -0300

    Alteração no nome da classe
    Colocar o nome da classe  no singular

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dto/Cidade.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dto/Cidade.java
@@ -9,12 +9,12 @@ package br.com.redesocial.modelo.dto;
  *
  * @author Daniel, Jônatas e Paulo
  */
-public class Cidades {
+public class Cidade {
     private int id;
-    private Estados estado;
+    private Estado estado;
     private String nome;
 
-    public Cidades(int id, int estado, String nome) {
+    public Cidade(int id, int estado, String nome) {
         this.id = id;
         this.estado = estado;
         this.nome = nome;
