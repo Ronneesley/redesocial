69c58c9e6fc3cd961cec3e780aadacbaa89e56cd
Jeferson Rossini <jrossini94@gmail.com>
2017-08-05 01:08:03 -0300

    Cria Método Excluir[Multimídia]
    Adiciona método de Exclusão.

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/MultimidiaDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/MultimidiaDAO.java
@@ -5,11 +5,30 @@
  */
 package br.com.redesocial.modelo.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author Lara
  */
 public class MultimidiaDAO {
     
+    /* METÓDO PARA CONECTAR AO BANCO DE DADOS*/
+    private Connection getConexao() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
+    
+    
+    public void excluir(int id) throws Exception {
+        Connection conexao = getConexao();
+        
+        PreparedStatement pstmt;
+        pstmt = conexao.prepareStatement("delete from multimidia where id = ?");
+        
+        pstmt.setInt(1, id);
+        pstmt.executeUpdate();
+    }
+
 }
