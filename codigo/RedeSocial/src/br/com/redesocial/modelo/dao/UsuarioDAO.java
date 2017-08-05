fa2da2dd4208a1a7b9b861c81b87867e46397739
Ronneesley <ronneesley@gmail.com>
2017-08-05 19:46:15 -0300

    Junção do arquivo de Usuário

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/UsuarioDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/UsuarioDAO.java
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-package br.com.redesocial.modelo.dao;
-
-import br.com.redesocial.modelo.dao.interfaces.DAOCRUD;
-import br.com.redesocial.modelo.dto.Usuario;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-
-/**
- * Objeto de acesso aos dados dos usuários
- * @author Ronneesley Moura Teles
- * @since 27/07/2017
- */
-public class UsuarioDAO extends DAOBase implements DAOCRUD<Usuario> {
-    /**
-     * Inseri um objeto no banco de dados na tabela usuários
-     * @param dto objeto com os dados de usuário já preenchido
-     * @throws Exception 
-     */
-    @Override
-    public void inserir(Usuario dto) throws Exception {
-        Connection conexao = getConexao();
-        
-        PreparedStatement pstmt = conexao.prepareStatement("insert into usuarios(nome, email, senha) values(?, ?, ?)");
-        
-        pstmt.setString(1, dto.getNome());
-        pstmt.setString(2, dto.getEmail());
-        pstmt.setString(3, dto.getSenha());
-        
-        pstmt.executeUpdate();
-    }
-}
-=======
 package br.com.redesocial.modelo.dao;
 
 import br.com.redesocial.modelo.dao.interfaces.DAOCRUD;
@@ -101,4 +68,3 @@ public class UsuarioDAO extends DAOBase implements DAOCRUD<Usuario> {
         System.out.println(u.getNome());
     }
 }
->>>>>>> ffd346fbf6d52688d309667a2af4542ef6ba775f
