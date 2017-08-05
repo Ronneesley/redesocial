f9c3dd9a364151c009ffbaa34016b2c29cd1c886
Ronneesley <ronneesley@gmail.com>
2017-08-05 19:59:49 -0300

    Novas dicas

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/MultimidiaDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/MultimidiaDAO.java
@@ -15,57 +15,55 @@ import java.sql.PreparedStatement;
  *
  * @author Lara, Jeferson
  */
-public class MultimidiaDAO extends DAOBase{
-    /* METÓDO PARA CONECTAR AO BANCO DE DADOS*/
-    
+public class MultimidiaDAO extends DAOBase {
     public Multimidia selecionar(int id) throws Exception{
         Connection conexao = getConexao();
-        
+
         PreparedStatement pstmt;
         pstmt = conexao.prepareStatement("select * from multimidia where id = ?");
         pstmt.setInt(1, id);
-        
+
         ResultSet rs;
         rs = pstmt.executeQuery();
-        
+
         if (rs.next()){
             Multimidia m = new Multimidia();
             m.setId(rs.getInt("id"));
             m.setMidia(rs.getBytes("midia"));
             m.setTipoConteudo(rs.getString("tipoConteudo"));
             m.setData(rs.getDate("data"));
-            
+
             return m;
         } else {
             return null;
         }
     }
-   
+
     public void inserir (Multimidia m) throws Exception {
         Connection conexao = getConexao();
-        
+
         //Arrumar as validações
         if (conexao.getMidia().trim().equals("")){
             throw new Exception("A mídia não pode estar vazia!");
         }
-        
+
         PreparedStatement pstmt;
         pstmt = conexao.prepareStatement("insert into multimidia(id, midia, tipoConteudo, data) values(?, ?, ?, ?)");
-        
-        pstmt.setString(1, conexao.getId());
+
+        pstmt.setString(1, conexao.getId()); //ID é automático, portanto não deve estar no SQL, nem arqui informado
         //Mídia
         //Conteúdo
         pstmt.setDate(4, new java.sql.Date(m.getData().getTime()));
 
         pstmt.executeUpdate();
     }
-    
+
     public void excluir(int id) throws Exception {
         Connection conexao = getConexao();
-        
+
         PreparedStatement pstmt;
         pstmt = conexao.prepareStatement("delete from multimidia where id = ?");
-        
+
         pstmt.setInt(1, id);
         pstmt.executeUpdate();
     }
