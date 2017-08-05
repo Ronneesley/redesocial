f04f6e86e5e737be12687c2eb12e2bb310d55ffe
Lara Caroline <laracaroline2007@hotmail.com>
2017-08-04 22:16:13 -0300

    Criação do DAO para a classe Multimidia método selecionar.

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/MultimidiaDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/MultimidiaDAO.java
@@ -5,11 +5,36 @@
  */
 package br.com.redesocial.modelo.dao;
 
+import br.com.redesocial.modelo.dto.Multimidia;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author Lara
  */
 public class MultimidiaDAO {
     
+    public Multimidia selecionar(int id) throws Exception{
+        
+        PreparedStatement pstmt;
+        pstmt = con.prepareStatement("select * from multimidia where id = ?");
+        pstmt.setInt(1, id);
+        
+        ResultSet rs;
+        rs = pstmt.executeQuery();
+        
+        if (rs.next()){
+            Multimidia m = new Multimidia();
+            m.setId(rs.getInt("id"));
+            m.setMidia(rs.getBytes("midia"));
+            m.setTipoConteudo(rs.getString("tipoConteudo"));
+            m.setData(rs.getDate("data"));
+            
+            return m;
+        } else {
+            return null;
+        }
+    }
     
 }
