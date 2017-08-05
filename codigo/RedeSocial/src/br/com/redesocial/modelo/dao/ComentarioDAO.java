5c249bcb9324f4f1d57425925a9c71826427ba2e
heitorSi <heitordesouza1000hv@gmail.com>
2017-08-04 22:05:54 -0300

    excluindo os comentarios

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/ComentarioDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/ComentarioDAO.java
@@ -2,6 +2,7 @@ package br.com.redesocial.modelo.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
@@ -16,9 +17,26 @@ public class ComentarioDAO {
      * @throws SQLException 
      * @throws java.lang.ClassNotFoundException caso não encontre o driver do banco de dados
      */
+    
+    Connection con;
     protected Connection getConexao() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
         
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/redesocial", "admin", "redesocial");
     }
+    
+    private void conectar() throws Exception {
+        String url = "jdbc:mysql://localhost:3306/redesocial";
+        con = DriverManager.getConnection(url, "root", "");
+    }
+    
+    public void excluir(int id) throws Exception {
+        conectar();
+        
+        PreparedStatement pstmt;
+        pstmt = con.prepareStatement("delete from comentarios where id = ?");
+        pstmt.setInt(1, id);
+        pstmt.executeUpdate();
+    }
+    
 }
