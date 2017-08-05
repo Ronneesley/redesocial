21c3735e3a680771933d53d70ec5820aa3b339b2
PAULOHENRIQUEARAUJO-IFGOIANO <pauloaraujo@github.com>
2017-08-04 23:58:49 -0300

    mainSelecionar
    Criação do método mainSelecionar

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/UsuarioDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/UsuarioDAO.java
@@ -30,34 +30,41 @@ public class UsuarioDAO extends DAOBase implements DAOCRUD<Usuario> {
         pstmt.executeUpdate();
     }
     
-        public Usuario selecionar (int id) throws Exception{
-        Connection conexao = getConexao();
+    public Usuario selecionar (int id) throws Exception{
+    Connection conexao = getConexao();
+
+    PreparedStatement  pstmt; 
+    pstmt = conexao.prepareStatement("SELECT * FROM usuarios WHERE id = ?");
+    pstmt.setInt(1, id);
+
+    ResultSet rs;
+    rs = pstmt.executeQuery();
+
+    if (rs.next()){
+        Usuario u = new Usuario();
+        u.setId(rs.getInt("id"));
+        u.setNome(rs.getString("nome"));
+        u.setEmail(rs.getString("email"));
+        u.setTelefone(rs.getString("telefone"));
+        u.setSenha(rs.getString("senha"));
+        u.setNascimento(rs.getDate("data"));
+        //u.setSexo(rs.getSexo("sexo"));//olhar esse aqui
+        //u.setDataCadastro(rs.getData("dataCadastro"));
+        u.setStatus(rs.getBoolean("status"));
+        //u.setFoto(rs.getMultimida("foto"));
+        //u.setCidade(rs.setCidade("cidade"));
+
+        return u;
+
+    }else{
+        return null;
+    }
+    }
+    
+    public static void mainSelecionar(String[] args) throws Exception {
+        UsuarioDAO dao = new UsuarioDAO();
+        Usuario u = dao.selecionar(1);
         
-        PreparedStatement  pstmt; 
-        pstmt = conexao.prepareStatement("SELECT * FROM usuarios WHERE id = ?");
-        pstmt.setInt(1, id);
-        
-        ResultSet rs;
-        rs = pstmt.executeQuery();
-        
-        if (rs.next()){
-            Usuario u = new Usuario();
-            u.setId(rs.getInt("id"));
-            u.setNome(rs.getString("nome"));
-            u.setEmail(rs.getString("email"));
-            u.setTelefone(rs.getString("telefone"));
-            u.setSenha(rs.getString("senha"));
-            u.setNascimento(rs.getDate("data"));
-            //u.setSexo(rs.getSexo("sexo"));//olhar esse aqui
-            //u.setDataCadastro(rs.getData("dataCadastro"));
-            u.setStatus(rs.getBoolean("status"));
-            //u.setFoto(rs.getMultimida("foto"));
-            //u.setCidade(rs.setCidade("cidade"));
-            
-            return u;
-            
-        }else{
-            return null;
-        }
+        System.out.println(u.getNome());
     }
 }
