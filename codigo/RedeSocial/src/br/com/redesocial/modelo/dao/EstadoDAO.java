7f775b5dd87de655111a2533218a6c7bab3fde1e
Igor Justino Rodrigues <igor98rodrigues@gmail.com>
2017-08-07 10:59:06 -0300

    Metodo Listar de EstadoDAO.java Pelo que entendi, acho que esta certo, tive que fazer uma pequena alteração no arquivo Estado.java, que não tinha o construtor vazio.

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/EstadoDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/EstadoDAO.java
@@ -1,8 +1,13 @@
 package br.com.redesocial.modelo.dao;
 
+import br.com.redesocial.modelo.dto.Estado;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Classe base para conexão com o banco de dados
@@ -21,4 +26,31 @@ public class EstadoDAO {
         
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/redesocial", "admin", "redesocial");
     }
+    
+    public List listar() throws Exception {
+        Connection conexao = getConexao();
+
+        PreparedStatement pstmt;
+        pstmt = conexao.prepareStatement("select * from estados order by id desc"); 
+
+        ResultSet rs;
+        rs = pstmt.executeQuery();
+
+        List lista;
+        lista = new ArrayList();
+        
+        //PaisDAO paisDAO = new PaisDAO();
+        while (rs.next()){
+            Estado e = new Estado();
+
+            e.setId(rs.getInt("id"));
+            e.setNome(rs.getString("nome"));
+            //e.setPais(paisDAO.selecionar(rs.getInt("pais")));  
+            
+            lista.add(e);
+        }
+
+        return lista;
+    }
+
 }
