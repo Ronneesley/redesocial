6225f2e00c59cfb52644a0c81ae99ddaf1871a78
Igor Justino Rodrigues <igor98rodrigues@gmail.com>
2017-08-08 12:13:09 -0300

    Criação da função alterar PostagemDAO. Tive uma duvida sobre como sera feita a alteração do Usuario.

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/PostagemDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/PostagemDAO.java
@@ -1,5 +1,6 @@
 package br.com.redesocial.modelo.dao;
 
+import br.com.redesocial.modelo.dto.Postagem;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 /**
@@ -15,6 +16,21 @@ public class PostagemDAO extends DAOBase {
         pstmt = conexao.prepareStatement("delete from postagens where id = ?");
         
         pstmt.setInt(1, id);
+        pstmt.executeUpdate();
+    }
+    
+    public void alterar(Postagem p) throws Exception{
+        Connection conexao = getConexao();
+        
+        PreparedStatement pstmt;
+        pstmt = conexao.prepareStatement("update postagens set curtidas = ?, descricao = ?, data = ?, usuario = ? where id = ?");
+        
+        pstmt.setInt(1, p.getCurtidas());
+        pstmt.setString(2, p.getDescricao());
+        pstmt.setDate(3, p.getData());
+        pstmt.setString(4, p.getUsuario());
+        pstmt.setInt(5, p.getId());
+        
         pstmt.executeUpdate();
     }
 }
