143f11bfb3be37205f53d1e986fa3c5502d3688b
PAULOHENRIQUEARAUJO-IFGOIANO <pauloaraujo@github.com>
2017-08-08 12:06:45 -0300

    Criação metodo listar
    Criação do método listar, deu um erro, acredito que possa ser de relacionamento entre as classes.

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/PostagemDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/PostagemDAO.java
@@ -3,6 +3,8 @@ package br.com.redesocial.modelo.dao;
 import br.com.redesocial.modelo.dto.Postagem;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -35,9 +37,34 @@ public class PostagemDAO extends DAOCRUDBase<Postagem> {
     public Postagem selecionar(int id) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //Apague a linha e escreva o código
     }
-
+    
+    /**
+    *
+    * @author Paulo Henrique
+    */
     @Override
     public List listar() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //Apague a linha e escreva o código
+        Connection conexao = getConexao();
+        
+        PreparedStatement pstmt;
+        pstmt = conexao.prepareStatement("select * from postagens order by data desc");
+        ResultSet rs;
+        rs = pstmt.executeQuery();
+        
+        List lista;
+        lista = new ArrayList();
+        
+        while(rs.next()){
+            Postagem p = new Postagem();
+            p.setId(rs.getInt("id"));
+            p.setCurtidas(rs.getInt("curtidas"));
+            p.setDescricao(rs.getString("descricao"));
+            p.setData(rs.getDate("data"));
+            p.usuarioDAO.listar(rs.getInt("usuario")); //deu erro aqui
+            lista.add(p);
+        }
+        
+        return lista;
+     
     }
 }
