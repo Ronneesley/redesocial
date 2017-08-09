3f5e8e92bda195b1719a6377172aa10c1dee1952
Daniel Moreira <qualquercoisa6568@hotmail.com>
2017-08-09 14:55:43 -0300

    Correções

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dto/PostagemMultimidia.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dto/PostagemMultimidia.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.redesocial.modelo.dto;
 
 /**
- *
- * @author qualq
+ * DTO para representação das Postagens com Multimídias
+ * @author Daniel e Macilon
  */
 public class PostagemMultimidia {
+    private Postagem postagem;
+    private Multimidia multimidia;
+
+    public PostagemMultimidia() {
+    
+    }
+    
+    public PostagemMultimidia(Postagem postagem, Multimidia multimidia) {
+        this.postagem = postagem;
+        this.multimidia = multimidia;
+    }
+    
+    public Postagem getPostagem() {
+        return postagem;
+    }
+
+    public void setPostagem(Postagem postagem) {
+        this.postagem = postagem;
+    }
+
+    public Multimidia getMultimidia() {
+        return multimidia;
+    }
+
+    public void setMultimidia(Multimidia multimidia) {
+        this.multimidia = multimidia;
+    }
+    
+    
     
 }
