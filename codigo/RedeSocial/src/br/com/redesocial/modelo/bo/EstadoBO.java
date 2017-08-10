4c21f89f519a0fddc8c1f365e95222a0ec015da1
Daniel Moreira <qualquercoisa6568@hotmail.com>
2017-08-10 15:24:08 -0300

    Criação da classe EstadoBO
    Mudei o campo do id da classe Estado para Integer, ao invés de int para aceitar a atribuição a null

---
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/bo/EstadoBO.java
@@ -0,0 +1,31 @@
+package br.com.redesocial.modelo.bo;
+
+import br.com.redesocial.modelo.dao.EstadoDAO;
+import br.com.redesocial.modelo.dto.Estado;
+
+/**
+ *
+ * @author Daniel Moreira Cardoso
+ */
+public class EstadoBO extends BOCRUDBase<Estado, EstadoDAO>{
+
+    @Override
+    protected EstadoDAO instanciarDAO() {
+        return new EstadoDAO();
+    }
+
+    @Override
+    protected void validarChavePrimaria(Estado dto) throws Exception {
+        if (dto.getId() == null) 
+            throw new Exception("Preencha o campo id.");
+    }
+
+    @Override
+    protected void validar(Estado dto) throws Exception {
+       if (dto.getNome().trim().equals(""))
+            throw new Exception("Insira o nome do estado.");
+    
+    }
+    
+    
+}
