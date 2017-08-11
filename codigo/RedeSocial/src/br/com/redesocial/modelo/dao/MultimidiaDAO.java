84c5a02c7632d775d189a104a811035f5dd8cf01
Lucianodcb <30538692+Lucianodcb@users.noreply.github.com>
2017-08-11 20:19:52 -0300

    correção

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/MultimidiaDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/MultimidiaDAO.java
@@ -86,7 +86,7 @@ public class MultimidiaDAO extends DAOCRUDBase<Multimidia> {
             Multimidia m = new Multimidia();
             m.setId(rs.getInt("id"));
             m.setMidia(rs.getBytes("midia"));
-            m.setTipoConteudo(rs.getString("tipoConteudo"));
+            m.setTipoConteudo(rs.getString("tipo_conteudo"));
             m.setData(rs.getDate("data"));
             lista.add(m);
         }
