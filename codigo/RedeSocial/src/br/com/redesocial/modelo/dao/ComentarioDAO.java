52246da3dee2ec6f2dd94c22ded0a97c25e91810
FernandoMacielS <fernandomacieloffice@gmail.com>
2017-08-11 16:46:48 -0300

    Re-Upload do ComentarioDAO
    Criação do DAO para a classe Comentario, método inserir.

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/ComentarioDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/ComentarioDAO.java
@@ -1,15 +1,34 @@
-
 package br.com.redesocial.modelo.dao;
 
 import br.com.redesocial.modelo.dao.interfaces.DAOCRUD;
-import br.com.redesocial.modelo.dto.Usuario;
+import br.com.redesocial.modelo.dto.Comentario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 /**
- *
+ * Objeto de acesso aos dados dos Comentarios
  * @author Fernando Maciel da Silva
+ * @since 11/08/2017
  */
-public class ComentarioDAO {
+
+public class ComentarioDAO extends DAOBase implements DAOCRUD<Comentario> {
+
+    /**
+     * Inseri um objeto no banco de dados na tabela comentario.
+     * @param dto objeto responsavel pela transferencia dos dados do comentario.
+     * @throws java.lang.Exception
+     */
     
+    @Override // envia uma mensagem de erro caso mude o nome do metodo pai.
+
+    public void inserir(Comentario dto) throws Exception {
+        
+        Connection conexao = getConexao();
+        
+        PreparedStatement pstmt = conexao.prepareStatement("insert into comentario(descricao) values(?)");
+        
+        pstmt.setString(1, dto.getDescricao());
+                
+        pstmt.executeUpdate();
+    }    
 }
