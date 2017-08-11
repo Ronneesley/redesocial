113a34ed240623c8ff5458d25c81fc9ceec0d440
EXOxShiro <30538550+EXOxShiro@users.noreply.github.com>
2017-08-11 16:48:26 -0300

    Update PostagemDAO.java

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/PostagemDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/PostagemDAO.java
@@ -34,7 +34,7 @@ public class PostagemDAO extends DAOCRUDBase<Postagem> {
         pstmt = conexao.prepareStatement("insert into postagens (curtidas,descricao,data) values (?,?,?)");
         
         pstmt.setInt(1, p.getCurtidas());
-        pstmt.setDescricao(2,p.getDescricao());
+        pstmt.setString(2,p.getDescricao());
         pstmt.setDate(3, new java.sql.Date(p.getData().getTime()));
         pstmt.executeUpdate();
         
