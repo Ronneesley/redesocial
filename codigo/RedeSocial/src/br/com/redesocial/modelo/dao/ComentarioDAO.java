37ea6ce33c2f16460f376f9f63007972779436d5
FernandoMacielS <fernandomacieloffice@gmail.com>
2017-08-11 11:48:02 -0300

    Criação do ComentarioDAO
    Criação do DAO para a Classe Comentario, método Inserir.

---
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/ComentarioDAO.java
@@ -0,0 +1,15 @@
+
+package br.com.redesocial.modelo.dao;
+
+import br.com.redesocial.modelo.dao.interfaces.DAOCRUD;
+import br.com.redesocial.modelo.dto.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author Fernando Maciel da Silva
+ */
+public class ComentarioDAO {
+    
+}
