fdd814255b093e27aaa617c5a4684717eb1b3a15
LuckasAzevedo <luckaskinha@gmail.com>
2017-08-11 13:41:49 -0300

    Criaçao CidadeBO

--- codigo/RedeSocial/src/br/com/redesocial/modelo/bo/CidadeBO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/bo/CidadeBO.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.redesocial.modelo.bo;
 
+import br.com.redesocial.modelo.dao.CidadeDAO;
+import br.com.redesocial.modelo.dto.Cidade;
+
 /**
- *
- * @author Cliente
+ * Define as regras de negócio para os usuários
+ * @author Ronneesley Moura Teles
+ * @since 27/07/2017
  */
-public class CidadeBO {
+public class CidadeBO extends BOCRUDBase<Cidade, CidadeDAO> {
+    @Override
+    protected CidadeDAO instanciarDAO() {
+        return new CidadeDAO();
+    }
+
+  
+    @Override
+    protected void validar(Cidade dto) throws Exception {
+        //Validações
+        if (dto.getNome().trim().equals("")) throw new Exception("Preencha o nome da cidade");
+        
+    }
+
     
+    @Override
+    protected void validarChavePrimaria(Cidade dto) throws Exception {
+        if (dto.getId() == null) throw new Exception("Preencha o campo id");
+    }
+    
+    protected void validarEstado(Cidade dto) throws Exception{
+        if (dto.getEstado().getNome().trim().equals(""))
+            throw new Exception("Insira o nome do estado.");
+
+    }
 }
+
