e273386a5067edbc4c96c748e6f281a7fbe18a88
aandreysr <aandreysr@gmail.com>
2017-08-11 15:34:27 -0300

    Classe AlbumBO

---
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/bo/AlbumBO.java
@@ -0,0 +1,32 @@
+package br.com.redesocial.modelo.bo;
+
+import br.com.redesocial.modelo.dao.AlbumDAO;
+import br.com.redesocial.modelo.dto.Album;
+
+
+/**
+ * Define as regras de negócio para os usuários
+ * @author Andrey Silva Ribeiro
+ * @since 11/08/2017
+ */
+public class AlbumBO extends BOCRUDBase<Album, AlbumDAO> {
+    @Override
+    protected AlbumDAO instanciarDAO() {
+        return new AlbumDAO();
+    }
+
+    @Override
+    protected void validar(Album dto) throws Exception {
+        //Validações
+        if (dto.getNome().trim().equals("")) throw new Exception("Preencha o nome do album");
+        
+        if (dto.getUsuario() == null) throw new Exception("Preencha o nome do usuário");
+    }
+    
+    @Override
+    protected void validarChavePrimaria(Album dto) throws Exception {
+        if (dto.getId() == null) throw new Exception("Preencha o campo id");
+    }
+
+    
+}
