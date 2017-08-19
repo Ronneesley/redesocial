adcd958828cbd90ef5314b48f6630349b9a9acba
Igor Justino Rodrigues <igor98rodrigues@gmail.com>
2017-08-19 11:33:42 -0300

    Criação do arquivo de Teste UsuarioBOTeste.java;

---
+++ codigo/RedeSocial/test/br/com/redesocial/modelo/bo/UsuarioBOTest.java
@@ -0,0 +1,31 @@
+package br.com.redesocial.modelo.bo;
+
+import org.junit.Test;
+
+/**
+ * Unidade de testes para o UsuarioBO
+ * @author Igor Justino Rodrigues
+ * @since 19/08/2017
+ */
+public class UsuarioBOTest {
+    @Test
+    public void testMetodoInserir() {
+    
+    }
+    
+    @Test
+    public void testMetodoListar() {
+
+    }
+   
+    @Test
+    public void testMetodoSelecionar() {
+    
+    }
+    
+    @Test
+    public void testMetodoExcluir() throws Exception{
+    
+    }
+   
+}
