89be4aa49028f5ab6c22cc50d9ed724fbdd46f0f
Lucianodcb <30538692+Lucianodcb@users.noreply.github.com>
2017-08-23 01:12:19 -0300

    teste excluir comentário(Não funciona)

--- codigo/RedeSocial/test/br/com/redesocial/modelo/bo/ComentarioBOTest.java
+++ codigo/RedeSocial/test/br/com/redesocial/modelo/bo/ComentarioBOTest.java
@@ -5,10 +5,21 @@
  */
 package br.com.redesocial.modelo.bo;
 
-import br.com.redesocial.modelo.dto.Comentario;
+import br.com.redesocial.modelo.dto.Postagem;
+import br.com.redesocial.modelo.dto.Album;
+import br.com.redesocial.modelo.dto.Cidade;
+import br.com.redesocial.modelo.dto.Estado;
+import br.com.redesocial.modelo.dto.Pais;
+import br.com.redesocial.modelo.dto.Usuario;
+import br.com.redesocial.modelo.dto.enumeracoes.Sexo;
+import br.com.redesocial.modelo.utilitarios.Utilitarios;
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import br.com.redesocial.modelo.dto.Comentario;
 /**
  *
  * @author Lara
@@ -80,6 +91,84 @@ public class ComentarioBOTest {
     }
     
     public void testMetodoExcluir(){
-        //INSIRA O testMetodoExcluir AQUI
+       ComentarioBO bo = new ComentarioBO();
+        
+       Comentario comentario = new Comentario();
+       comentario.setId(1);
+       
+       try{
+           
+            PaisBO paisBO = new PaisBO();
+            Pais pais = null;
+            paisBO.inserir(pais);
+
+            Estado estado = new Estado();
+            estado.setNome("California");
+            estado.setPais(pais);
+            
+            EstadoBO estadoBO = new EstadoBO();
+            estadoBO.inserir(estado);
+            
+            Cidade cidade = new Cidade();
+            cidade.setNome("Los Angeles");
+            cidade.setEstado(estado);
+            
+            CidadeBO cidadeBO = new CidadeBO();
+            cidadeBO.inserir(cidade);
+            
+            Usuario usuario = new Usuario();
+            usuario.setNome("Raul");
+            usuario.setDataCadastro(new Date());
+            usuario.setEmail("raul@gmail.com");
+            //usuario.setFoto();
+           
+            Calendar calendario = Calendar.getInstance();
+            calendario.set(1986, 2, 8, 0, 0, 0);            
+            usuario.setDataNascimento(calendario.getTime());
+            usuario.setSenha("1239");
+            usuario.setSexo(Sexo.MASCULINO);
+            usuario.setStatus(true);
+            usuario.setTelefone("(62) 91432-9867");
+            usuario.setCidade(cidade);
+            
+            UsuarioBO usuarioBO = new UsuarioBO();
+            usuarioBO.inserir(usuario);
+           
+
+            Postagem postagem = new Postagem();
+            postagem.setDescricao("Comentário");
+            postagem.setCurtidas(0);
+            postagem.setUsuario(usuario);
+
+            calendario.set(2017, 7, 16, 21, 58, 0);
+            postagem.setData(calendario.getTime());
+            
+            PostagemBO postagemBO  = new PostagemBO();
+            postagemBO.inserir(postagem);
+            
+ 
+           
+            comentario.setDescricao("bommm");
+            comentario.setCurtidas(0);
+            
+            calendario.set(1986, 4, 8, 0, 0, 0);            
+            comentario.setData(calendario.getTime());
+            comentario.setPostagem(1);
+            
+            bo.inserir(comentario);
+           
+            int id = comentario.getId();
+            Comentario comentarioSelecionado = bo.selecionar(id);
+            assertNotNull("Album não encontrada", comentarioSelecionado);
+          
+            bo.excluir(id);
+           
+           Comentario comentarioSelecionadoPosExclusao = bo.selecionar(id);
+           
+           assertNull("comentario não encontrado", comentarioSelecionadoPosExclusao);
+        }catch (Exception ex){
+                fail ("Falha ao inserir um comentario" + ex.getMessage());
+                
+         }
     }
 }
