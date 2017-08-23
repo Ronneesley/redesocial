a6cd88429e3d43534efc932452afde57fb74e242
Lara Caroline <laracaroline2007@hotmail.com>
2017-08-23 19:22:22 -0300

    Mudanças ComentarioBOTest
    Porém não funcionou ainda :( ME AJUDA

--- codigo/RedeSocial/test/br/com/redesocial/modelo/bo/ComentarioBOTest.java
+++ codigo/RedeSocial/test/br/com/redesocial/modelo/bo/ComentarioBOTest.java
@@ -5,7 +5,15 @@
  */
 package br.com.redesocial.modelo.bo;
 
+import br.com.redesocial.modelo.dto.Cidade;
 import br.com.redesocial.modelo.dto.Comentario;
+import br.com.redesocial.modelo.dto.Estado;
+import br.com.redesocial.modelo.dto.Pais;
+import br.com.redesocial.modelo.dto.Postagem;
+import br.com.redesocial.modelo.dto.Usuario;
+import br.com.redesocial.modelo.dto.enumeracoes.Sexo;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -47,7 +55,7 @@ public class ComentarioBOTest {
 
     }
     
-    
+    @Test
     public void testMetodoListar() {
         ComentarioBO bo = new ComentarioBO();
         
@@ -55,10 +63,73 @@ public class ComentarioBOTest {
             List existentes = bo.listar();
             int qtdeExistentes = existentes.size();
             
-            final int qtde = 10;
-            for (int i = 0; i < 10; i++){
+            Calendar calendario = Calendar.getInstance();
+            calendario.set(2017, 7, 18, 10, 55, 13);
+            
+            Calendar calendarioPost = Calendar.getInstance();
+            calendarioPost.set(2017, 7, 18, 9, 30, 45);
+            
+            Calendar calendarioNascimento = Calendar.getInstance();
+            calendarioNascimento.set(2017, 7, 18, 9, 30, 45);
+            
+            Pais pais = new Pais();
+            
+            pais.setNome("Brasil");
+            
+            PaisBO paisBO = new PaisBO();
+            paisBO.inserir(pais);
+            
+            Estado estado = new Estado();
+            
+            estado.setNome("Goiás");
+            estado.setPais(pais);
+            
+            EstadoBO estadoBO = new EstadoBO();
+            estadoBO.inserir(estado);
+            
+            Cidade cidade = new Cidade();
+            
+            cidade.setNome("Goiânia");
+            cidade.setEstado(estado);
+            
+            CidadeBO cidadeBO = new CidadeBO();
+            cidadeBO.inserir(cidade);
+            
+            Usuario usuario = new Usuario();
+            
+            usuario.setNome("Joana");
+            usuario.setDataCadastro(new Date());
+            usuario.setEmail("joaninha@contato.com");
+            //usuario.setFoto();           
+            usuario.setDataNascimento(calendarioNascimento.getTime());
+            usuario.setSenha("192837");
+            usuario.setSexo(Sexo.FEMININO);
+            usuario.setStatus(true);
+            usuario.setTelefone("(62) 91234-4567");
+            usuario.setCidade(cidade);
+            
+            UsuarioBO usuarioBO = new UsuarioBO();
+            usuarioBO.inserir(usuario);
+            
+            Postagem postagem = new Postagem();
+            
+            postagem.setCurtidas(6);
+            postagem.setDescricao("Post de texto");
+            postagem.setData(calendarioPost.getTime());
+            postagem.setUsuario(usuario);
+            
+            PostagemBO postagemBO = new PostagemBO();
+            postagemBO.inserir(postagem);
+            
+            final int qtde = 3;
+            for (int i = 0; i < 3; i++){
+                
                 Comentario comentario = new Comentario();
-                comentario.setDescricao("Comentario");
+                
+                comentario.setDescricao("Que legal!");
+                comentario.setCurtidas(2);
+                comentario.setData(calendario.getTime());
+                comentario.setPostagem(postagem);
                 
                 try{
                     bo.inserir(comentario);
