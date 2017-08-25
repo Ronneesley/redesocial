0b38e18f8f534b4a47588caeeaad979489228e43
aandreysr <aandreysr@gmail.com>
2017-08-25 15:23:23 -0300

    Documentação PostagemAlbumDTO

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dto/PostagemAlbum.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dto/PostagemAlbum.java
@@ -5,30 +5,58 @@ package br.com.redesocial.modelo.dto;
  * @author Andrey
  */
 public class PostagemAlbum {
+    //classe privadas de postagem e album
     private Postagem postagem;
     private Album album;
 
+    
+    /**
+     * Construtor vazio para a classe
+     */
     public PostagemAlbum() {
     
     }
     
+    /**
+     * Construtor da classe Pais com dois parâmetros
+     * @param postagem identificador da postagem
+     * @param album nome desejado para o album
+     */
     public PostagemAlbum(Postagem postagem, Album album) {
         this.postagem = postagem;
         this.album = album;
     }
     
+    /**
+     * Função que retorna o identificador de uma postagem
+     * @return identificador
+     */
     public Postagem getPostagem() {
         return postagem;
     }
 
+    
+    /**
+     * Função que modifica o nome da postagem
+     * @param postagem novo nome da postagem
+     */
     public void setPostagem(Postagem postagem) {
         this.postagem = postagem;
     }
 
+    /**
+     * Função que retorna o identificador de umalbum
+     * @return identificador
+     */
     public Album getAlbum() {
         return album;
     }
 
+    
+    /**
+     * Função que modifica o nome do album
+     * @param album novo nome do album
+     */
     public void setAlbum(Album album) {
         this.album = album;
     }
