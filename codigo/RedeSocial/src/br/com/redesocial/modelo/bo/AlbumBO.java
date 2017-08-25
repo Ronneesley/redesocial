54bc624ab4c9667b7c54924258bced5f1b290250
aandreysr <aandreysr@gmail.com>
2017-08-25 15:18:29 -0300

    Documentação AlbumBO

--- codigo/RedeSocial/src/br/com/redesocial/modelo/bo/AlbumBO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/bo/AlbumBO.java
@@ -4,16 +4,27 @@ import br.com.redesocial.modelo.dao.AlbumDAO;
 import br.com.redesocial.modelo.dto.Album;
 
 /**
- * Define as regras de negócio para os usuários
  * @author Andrey Silva Ribeiro
  * @since 11/08/2017
  */
 public class AlbumBO extends BOCRUDBase<Album, AlbumDAO> {
+    
+    /**
+     * Instancia um objeto de album DAO
+     * @return instância de acesso aos dados do país
+     */
+    
     @Override
     protected AlbumDAO instanciarDAO() {
         return new AlbumDAO();
     }
 
+    /**
+     * Validação dos atributos do album
+     * @param dto objeto em questão que será validado
+     * @throws Exception validações encontradas
+     */
+    
     @Override
     protected void validar(Album dto) throws Exception {
         //Validações
@@ -23,6 +34,12 @@ public class AlbumBO extends BOCRUDBase<Album, AlbumDAO> {
         if (dto.getUsuario() == null) throw new Exception("Preencha o nome do usuário");
     }
 
+    /**
+     * Validação da chave primária de um objeto album
+     * @param dto objeto em questão
+     * @throws Exception validação se a chave não foi preenchida
+     */
+    
     @Override
     protected void validarChavePrimaria(Album dto) throws Exception {
         if (dto.getId() == null) throw new Exception("Preencha o campo id");
