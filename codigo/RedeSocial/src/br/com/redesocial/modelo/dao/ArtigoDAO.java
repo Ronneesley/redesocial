8667b7441a6a0173db4757d033237701fad1b5ab
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2017-08-26 16:04:13 -0300

    Listar do ArtigoDAO

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/ArtigoDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/ArtigoDAO.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.redesocial.modelo.dao;
+
+import br.com.redesocial.modelo.dto.Artigo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+//import java.sql.Statement;
 
 /**
  *
- * @author gusttavo nunes
+ * @author Gusttavo Nunes
  */
 public class ArtigoDAO {
-    
+    @Override
+    public void inserir(){
+        
+    }
+    @Override
+    public void selecionar(){
+        
+    }
+    @Override
+    public void excluir(){
+        
+    }
+    /**
+     * Método que lista todos os artigos em ordem descrecem por data do banco de dados
+     * @author Gusttavo Nunes Gomes
+     * @return lista os artigos em ordem descrescente por data
+     * @throws Exception possíveis exceções que podem acontecer
+     */
+    @Override
+    public List listar() throws Exception {
+        Connection conexao = getConexao();
+        
+        PreparedStatement pstmt;
+        pstmt = conexao.prepareStatement("select * from artigos order by data desc");
+        
+        ResultSet rs;
+        rs = pstmt.executeQuery();
+        
+        List lista;
+        lista = new ArrayList();
+        
+        while (rs.next()){
+            
+            Artigo a = new Artigo();
+            a.setId(rs.getInt("id"));
+            a.setIdioma(rs.getString("idioma"));
+            a.setRevista(rs.getString("revista"));
+            a.setISSN(rs.getString("issn"));
+            a.setAutor(rs.getString("autor"));
+            a.setData(rs.getDate("data"));
+            a.setAreaConhecimento(rs.getString("areaConhecimento"));
+            a.setTitulo(rs.getString("titulo"));
+            a.setResumo(rs.getString("resumo"));
+            a.setURL(rs.getString("URL"));
+            a.setArtigo(rs.getBytes("artigo"));    
+            lista.add(a);
+        }
+        
+        return lista;
+        
+    }
+    @Override
+    public void alterar(){
+        
+    }
 }
