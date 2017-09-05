21c85590f4f4b747de54f13259a6a1bf0f4b3601
Daniel Moreira <qualquercoisa6568@hotmail.com>
2017-09-05 16:07:06 -0300

    Criação da view para verificar a quantidade de novos usuários por mês e ano

--- banco_de_dados/views.sql
+++ banco_de_dados/views.sql
@@ -1,2 +1,14 @@
 /* Todos os códigos das views serão adicionados aqui */
-
+/**
+ * View para selecionar a quantidade de usuários cadastrados por mês
+ * @author Daniel Moreira Cardoso
+ */
+ 
+ CREATE VIEW `quantidade_de_usuarios_por_mes` AS
+    (SELECT 
+        DATE_FORMAT(`usuarios`.`data_cadastro`, '%M/%Y') AS `Mês_Ano`,
+        COUNT(*) AS `Quantidade de novos usuários`
+    FROM
+        `usuarios`
+    GROUP BY `Mês_Ano`
+    ORDER BY `usuarios`.`data_cadastro`);
