26a3b307e783ad50fa9ebd2af4dd8867aa3eeae4
PAULOHENRIQUEARAUJO-IFGOIANO <pauloaraujo@github.com>
2017-09-08 11:04:38 -0300

    Servelet
    Aparentemente pronto

--- codigo/RedeSocialWeb/src/java/br/com/redesocial/controle/EstadoControle.java
+++ codigo/RedeSocialWeb/src/java/br/com/redesocial/controle/EstadoControle.java
@@ -35,6 +35,14 @@ public class EstadoControle extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    /**
+     * Processa todas as requisições sejam elas do tipo GET ou POST
+     * @param request requisição
+     * @param response resposta
+     * @throws javax.servlet.ServletException se ocorre um erro no Servlet
+     * @throws java.io.IOException se ocorre um erro de entrada e saída
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -49,12 +57,26 @@ public class EstadoControle extends HttpServlet {
                 case "Cadastrar":
                     this.cadastrar(request, response);
                     break;
+                case "Listar":
+                    this.listar(request, response);
+                    break;
+                case "Excluir":
+                    this.excluir(request, response);
+                    break;
+                case "Editar":
+                    this.editar(request, response);
+                    break;
             }
         } catch (Exception ex){
 
         }
     }
-
+    /**
+     * Cria um novo estado
+     * @param request requisição
+     * @param response resposta
+     * @throws Exception
+     */
     private void criarNovo(HttpServletRequest request, HttpServletResponse response) throws Exception{
         try {
             Estado estado = new Estado();
@@ -104,8 +126,12 @@ public class EstadoControle extends HttpServlet {
             this.alterar(estado, request, response);
         }
     }
-
-
+    /**
+     * Insere um estado no banco de dados
+     * @param request
+     * @param response
+     * @throws Exception
+     */
     private void inserir(Estado estado, HttpServletRequest request, HttpServletResponse response) throws Exception{
         try {
             EstadoBO estadoBO = new EstadoBO();
@@ -119,9 +145,84 @@ public class EstadoControle extends HttpServlet {
         RequestDispatcher rd = request.getRequestDispatcher("paginas/estados/cadastro_estados.jsp");
         rd.forward(request, response);
     }
-
+    
+    /**
+     * Altera um estado no banco de dados
+     * @param request
+     * @param response
+     * @throws Exception
+     */
     private void alterar(Estado estado, HttpServletRequest request, HttpServletResponse response) throws Exception{
-        //Para vocês codificarem
+        try {
+            EstadoBO estadoBO = new EstadoBO();
+            estadoBO.alterar(estado);
+
+            request.setAttribute("mensagem", "Alteração realizada com sucesso");
+        } catch (Exception ex){
+            request.setAttribute("erro", ex.getMessage());
+        }
+    }
+    /**
+     * Lista todos os estados no banco de dados
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+    private void listar(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        try {
+            EstadoBO bo = new EstadoBO();
+            List estados = bo.listar();
+
+            request.setAttribute("lista", estados);
+        } catch (Exception ex){
+            request.setAttribute("erro", ex.getMessage());
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("paginas/paises/listagem_estados.jsp");
+        rd.forward(request, response);
+    }
+    /**
+     * Exclui um estado no banco de dados
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+    private void excluir(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        try {
+            Integer id = Integer.parseInt(request.getParameter("id"));
+
+            EstadoBO bo = new EstadoBO();
+            bo.excluir(id);
+
+            request.setAttribute("mensagem", "Excluído com sucesso");
+        } catch (Exception ex){
+            request.setAttribute("erro", ex.getMessage());
+        }
+
+        this.listar(request, response);
+    }
+    /**
+     * Edita um estado no banco de dados
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+    private void editar(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        try {
+            Integer id = Integer.parseInt(request.getParameter("id"));
+
+            EstadoBO estadoBO = new EstadoBO();
+            Estado estado = estadoBO.selecionar(id);
+
+            request.setAttribute("estado", estado);
+
+            request.setAttribute("mensagem", "Registro selecionado com sucesso");
+        } catch (Exception ex){
+            request.setAttribute("erro", ex.getMessage());
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("paginas/paises/cadastro_estado.jsp");
+        rd.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
