82ec2e1482f720a7ea2052a0bc3c6f262ef477b8
Daniel Moreira Cardoso <qualquercoisa6568@hotmail.com>
2017-09-15 19:51:53 -0300

    Criação do método selecionar de PalavraChaveDAO, já comentado

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/PalavraChaveDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/PalavraChaveDAO.java
@@ -8,6 +8,7 @@ package br.com.redesocial.modelo.dao;
 import br.com.redesocial.modelo.dto.PalavraChave;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.List;
 
@@ -36,9 +37,35 @@ public class PalavraChaveDAO extends DAOCRUDBase<PalavraChave> {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Método para selecionar uma palavra chave
+     * @param id identificador de palavra chave
+     * @return palavra chave ou null
+     * @throws Exception possíves erros que podem acontecer
+     */
     @Override
     public PalavraChave selecionar(int id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //conecta ao banco
+        Connection conexao = getConexao();
+        //prepara um comando sql
+        PreparedStatement pstmt;
+        pstmt = conexao.prepareStatement("select * from palavras_chave where id = ?");
+        
+        pstmt.setInt(1, id);
+        //executa o comando sql
+        ResultSet rs = pstmt.executeQuery();
+        
+        if(rs.next()){
+            //objeto para uma palavra chave
+            PalavraChave dto = new PalavraChave();
+            
+            dto.setId(rs.getInt("id"));
+            dto.setDescricao(rs.getString("descricao"));
+            //retorna o objeto desejado
+            return dto;
+        }else{       
+            return null;
+        }
     }
 
     @Override
