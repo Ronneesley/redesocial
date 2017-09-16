e2a0011d4307c1970264ea437f518785af82b521
LuckasAzevedo <luckaskinha@gmail.com>
2017-09-16 14:55:59 -0300

    Comentario codigo Aporte DTO

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dto/Aporte.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dto/Aporte.java
@@ -1,4 +1,3 @@
-
 package br.com.redesocial.modelo.dto;
 
 /**
@@ -11,36 +10,44 @@ public class Aporte {
      * Atributo que identifica aporte
      */
     private Integer id;
+    
     /**
      * Atributo que armazena o titulo do aporte
      */
     private String titulo;
+    
     /**
      * Atributo que armazena o categoria do aporte
      */
     private Categoria categoria;
+    
     /**
      * Atributo que armazena a postagem do aporte
      */
      private Postagem postagem;
+     
      /**
      * Construtor vazio para a classe
      */
     public Aporte(){
     } 
+    
     /**
      * Função que retorna o identificador de aporte
      * @return identificador
      */
+    
     public Integer getId(){
         return id;
     }
+    
     /**
      * Função que modifica o identificador de aporte
      * @param id novo identificador
      */
     public void setId(Integer id){
         this.id = id;
+        
      /**
      * Função que retorna o titulo do aporte
      * @return titulo do aporte
@@ -48,6 +55,7 @@ public class Aporte {
     }
     public void setTitulo(String titulo){
         this.titulo = titulo;
+        
     }   
     /**
      * Função que retorna categoria responsável pela criação do aporte
@@ -55,6 +63,7 @@ public class Aporte {
      */
     public Categoria getCategoria(){
         return categoria;
+        
     }
     /**
      * Função que modifica a categoria do aporte
@@ -63,6 +72,7 @@ public class Aporte {
     public void setCategoria(Categoria categoria){
         this.categoria = categoria;   
     }
+    
     /**
      * Função que retorna a postagem do aporte
      * @return postagem do aporte
@@ -70,6 +80,7 @@ public class Aporte {
     public Postagem getPostagem(){
         return postagem;
     }
+    
     /**
      * Função que modifica a postagem  do aporte
      * @param  postagem  nova postagem  do aporte
