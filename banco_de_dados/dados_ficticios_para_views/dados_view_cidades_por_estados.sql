ae9781afc3ebda187c7935c6ccdf5d18e85668ae
adallberto <30481941+adallberto@users.noreply.github.com>
2017-09-15 21:21:50 -0300

    Dados para view

---
+++ banco_de_dados/dados_ficticios_para_views/dados_view_cidades_por_estados.sql
@@ -0,0 +1,20 @@
+/*inserindo paises*/
+INSERT INTO `paises` VALUES 
+(1,'Brasil');
+/*inserindo estados*/
+INSERT INTO `estados` VALUES 
+(1,'Goiás',1),
+(2,'Minas Gerais',1),
+(3,'Tocantins',1),
+(4,'São Paulo',1);
+/*inserindo cidades*/
+INSERT INTO `cidades` VALUES 
+(1,'Ceres',1),
+(2,'Rialma',1),
+(3,'Goiânia',1),
+(4,'Guaxupé',2),
+(5,'Jacuí',2),
+(6,'Palmas',3),
+(7,'Gurupi',3),
+(8,'São Paulo',4),
+(9,'Franca',4);
