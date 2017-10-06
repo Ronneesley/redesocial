b5dad92a84ce46c31a675071276ebb170a9c67f7
Brener Gomes <30538550+EXOxShiro@users.noreply.github.com>
2017-10-06 20:18:32 -0300

    Update View_Aportes_por_Quantidade.sql

--- banco_de_dados/View_Aportes_por_Quantidade.sql
+++ banco_de_dados/View_Aportes_por_Quantidade.sql
@@ -1,6 +1,5 @@
-//View Aportes por Quantidade
-CREATE VIEW `AportesPorQuantidade` AS
-SELECT categorias.descricao, COUNT(*)
-FROM aportes, categorias
-where aportes.categoria = categorias.id
-GROUP BY aportes.categoria;
+CREATE VIEW `redesocial`.`aportesporquantidade` AS
+SELECT `redesocial`.`categorias`.`descricao` AS `CATEGORIA`,
+COUNT(0) AS `QUANTIDADE` FROM (`redesocial`.`aportes` JOIN `redesocial`.`categorias`)
+WHERE (`redesocial`.`aportes`.`categoria` = `redesocial`.`categorias`.`id`)
+GROUP BY `redesocial`.`aportes`.`categoria`
