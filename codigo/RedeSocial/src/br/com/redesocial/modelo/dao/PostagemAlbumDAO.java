b47c595bcdc50e57d94f081e8eccfd134ce9c935
Lara Caroline <laracaroline2007@hotmail.com>
2017-10-10 13:30:09 -0300

    Correção PostagemAlbumDAO

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/PostagemAlbumDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/PostagemAlbumDAO.java
@@ -1,10 +1,10 @@
 package br.com.redesocial.modelo.dao;
 
-import br.com.redesocial.modelo.dto.Postagem;
 import br.com.redesocial.modelo.dto.PostagemAlbum;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,18 +14,38 @@ import java.util.List;
  */
 public class PostagemAlbumDAO extends DAOBase {
     
-    public void excluir(int id) throws Exception {
+    public void excluir(int postagem, int album) throws Exception {
         Connection conexao = getConexao();
         
         PreparedStatement pstmt;
-        pstmt = conexao.prepareStatement("delete from postagens_albuns where id = ?");
+        pstmt = conexao.prepareStatement("delete from postagens_albuns where postagem = ? and album = ?");
         
-        pstmt.setInt(1, id);
+        pstmt.setInt(1, postagem);
+        pstmt.setInt(2, album);
+        
         pstmt.executeUpdate();
     }
 
-    public void inserir(Object dto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /**
+    * Método para inserir uma postagem com album
+    * @author Lara Caroline
+    * @throws Exception possíveis exceções que podem acontecer
+    * @param dto identificador de PostagemAlbum
+    */
+    public void inserir(PostagemAlbum dto) throws Exception {
+        Connection conexao = getConexao();
+       
+        if(dto.getPostagem() == null || dto.getAlbum() == null){
+            throw new Exception("A Postagem ou Album está vazia.");
+        }
+       
+        PreparedStatement pstmt;
+        pstmt = conexao.prepareStatement("insert into postagens_albuns (postagem, album) values (?,?)", Statement.RETURN_GENERATED_KEYS);
+       
+        pstmt.setInt(1, dto.getPostagem().getId());
+        pstmt.setInt(2, dto.getAlbum().getId());
+       
+        pstmt.executeQuery();
     }
 
     /**
@@ -45,33 +65,30 @@ public class PostagemAlbumDAO extends DAOBase {
         pstmt.setInt(3, dto.getPostagem().getId());
         pstmt.setInt(4, dto.getAlbum().getId());
         
-        pstmt.executeQuery();
+        pstmt.executeUpdate();
         
     }
 
-    public Postagem selecionar(int id) throws Exception{
+    public PostagemAlbum selecionar(int postagem, int album) throws Exception{
        Connection conexao = getConexao();
         
         PreparedStatement pstmt;
-        pstmt = conexao.prepareStatement("select * from postagens_albuns where id = ?");
+        pstmt = conexao.prepareStatement("select * from postagens_albuns where postagem = ? and album = ?");
         
-        pstmt.setInt(1, id);
+        pstmt.setInt(1, postagem);
+        pstmt.setInt(2, album);
         
         ResultSet rs = pstmt.executeQuery();
         
         if(rs.next()){
-            Postagem p = new Postagem();
-            UsuarioDAO usuarioDAO = new UsuarioDAO();
+            PostagemAlbum pa = new PostagemAlbum();
+            PostagemDAO postagemDAO = new PostagemDAO();
+            AlbumDAO albumDAO = new AlbumDAO();
             
-            p.setId(rs.getInt("id"));
-            p.setUps(rs.getInt("ups"));
-            p.setUps(rs.getInt("downs"));
-            p.setDescricao(rs.getString("descricao"));
-            p.setData(rs.getDate("data"));
+            pa.setPostagem(postagemDAO.selecionar(rs.getInt("postagem")));
+            pa.setAlbum(albumDAO.selecionar(rs.getInt("album")));
             
-            p.setUsuario(usuarioDAO.selecionar(rs.getInt("usuario")));
-            
-            return p;
+            return pa;
         } else {
             return null;
         } 
