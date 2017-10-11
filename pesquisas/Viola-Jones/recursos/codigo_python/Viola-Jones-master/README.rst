0f47b84eb701a3bd67c03be219db9f2f1937357d
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2017-10-11 10:16:53 -0300

    Inclusão de códigos de Python no Latex

---
+++ pesquisas/Viola-Jones/recursos/codigo_python/Viola-Jones-master/README.rst
@@ -0,0 +1,16 @@
+Viola Jones
+############
+
+Python implementation of the face detection algorithm by Paul Viola and Michael J. Jones.
+
+Install
+=======
+
+Run:
+
+    pip install -e <path to viola-jones>
+
+How to use
+==========
+
+See either ``example.py`` or Jupyter notebook ``Viola-Jones Example.ipynb``.
---
+++ pesquisas/Viola-Jones/recursos/codigo_python/Viola-Jones-master/violajones/HaarLikeFeature.py
@@ -0,0 +1,89 @@
+import violajones.IntegralImage as ii
+
+
+def enum(**enums):
+    return type('Enum', (), enums)
+
+FeatureType = enum(TWO_VERTICAL=(1, 2), TWO_HORIZONTAL=(2, 1), THREE_HORIZONTAL=(3, 1), THREE_VERTICAL=(1, 3), FOUR=(2, 2))
+FeatureTypes = [FeatureType.TWO_VERTICAL, FeatureType.TWO_HORIZONTAL, FeatureType.THREE_VERTICAL, FeatureType.THREE_HORIZONTAL, FeatureType.FOUR]
+
+
+class HaarLikeFeature(object):
+    """
+    Class representing a haar-like feature.
+    """
+
+    def __init__(self, feature_type, position, width, height, threshold, polarity):
+        """
+        Creates a new haar-like feature.
+        :param feature_type: Type of new feature, see FeatureType enum
+        :type feature_type: violajonse.HaarLikeFeature.FeatureTypes
+        :param position: Top left corner where the feature begins (x, y)
+        :type position: (int, int)
+        :param width: Width of the feature
+        :type width: int
+        :param height: Height of the feature
+        :type height: int
+        :param threshold: Feature threshold
+        :type threshold: float
+        :param polarity: polarity of the feature -1 or 1
+        :type polarity: int
+        """
+        self.type = feature_type
+        self.top_left = position
+        self.bottom_right = (position[0] + width, position[1] + height)
+        self.width = width
+        self.height = height
+        self.threshold = threshold
+        self.polarity = polarity
+        self.weight = 1
+    
+    def get_score(self, int_img):
+        """
+        Get score for given integral image array.
+        :param int_img: Integral image array
+        :type int_img: numpy.ndarray
+        :return: Score for given feature
+        :rtype: float
+        """
+        score = 0
+        if self.type == FeatureType.TWO_VERTICAL:
+            first = ii.sum_region(int_img, self.top_left, (self.top_left[0] + self.width, int(self.top_left[1] + self.height / 2)))
+            second = ii.sum_region(int_img, (self.top_left[0], int(self.top_left[1] + self.height / 2)), self.bottom_right)
+            score = first - second
+        elif self.type == FeatureType.TWO_HORIZONTAL:
+            first = ii.sum_region(int_img, self.top_left, (int(self.top_left[0] + self.width / 2), self.top_left[1] + self.height))
+            second = ii.sum_region(int_img, (int(self.top_left[0] + self.width / 2), self.top_left[1]), self.bottom_right)
+            score = first - second
+        elif self.type == FeatureType.THREE_HORIZONTAL:
+            first = ii.sum_region(int_img, self.top_left, (int(self.top_left[0] + self.width / 3), self.top_left[1] + self.height))
+            second = ii.sum_region(int_img, (int(self.top_left[0] + self.width / 3), self.top_left[1]), (int(self.top_left[0] + 2 * self.width / 3), self.top_left[1] + self.height))
+            third = ii.sum_region(int_img, (int(self.top_left[0] + 2 * self.width / 3), self.top_left[1]), self.bottom_right)
+            score = first - second + third
+        elif self.type == FeatureType.THREE_VERTICAL:
+            first = ii.sum_region(int_img, self.top_left, (self.bottom_right[0], int(self.top_left[1] + self.height / 3)))
+            second = ii.sum_region(int_img, (self.top_left[0], int(self.top_left[1] + self.height / 3)), (self.bottom_right[0], int(self.top_left[1] + 2 * self.height / 3)))
+            third = ii.sum_region(int_img, (self.top_left[0], int(self.top_left[1] + 2 * self.height / 3)), self.bottom_right)
+            score = first - second + third
+        elif self.type == FeatureType.FOUR:
+            # top left area
+            first = ii.sum_region(int_img, self.top_left, (int(self.top_left[0] + self.width / 2), int(self.top_left[1] + self.height / 2)))
+            # top right area
+            second = ii.sum_region(int_img, (int(self.top_left[0] + self.width / 2), self.top_left[1]), (self.bottom_right[0], int(self.top_left[1] + self.height / 2)))
+            # bottom left area
+            third = ii.sum_region(int_img, (self.top_left[0], int(self.top_left[1] + self.height / 2)), (int(self.top_left[0] + self.width / 2), self.bottom_right[1]))
+            # bottom right area
+            fourth = ii.sum_region(int_img, (int(self.top_left[0] + self.width / 2), int(self.top_left[1] + self.height / 2)), self.bottom_right)
+            score = first - second - third + fourth
+        return score
+    
+    def get_vote(self, int_img):
+        """
+        Get vote of this feature for given integral image.
+        :param int_img: Integral image array
+        :type int_img: numpy.ndarray
+        :return: 1 iff this feature votes positively, otherwise -1
+        :rtype: int
+        """
+        score = self.get_score(int_img)
+        return self.weight * (1 if score < self.polarity * self.threshold else -1)
