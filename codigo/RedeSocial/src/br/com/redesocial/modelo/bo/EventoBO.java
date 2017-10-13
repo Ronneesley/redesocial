679ba511e007223aea50598c6b08609324233734
willianIF <30538522+willianIF@users.noreply.github.com>
2017-10-13 18:13:55 -0300

    Criação das regras de negocio de EventoBO

--- codigo/RedeSocial/src/br/com/redesocial/modelo/bo/EventoBO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/bo/EventoBO.java
@@ -1,15 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.redesocial.modelo.bo;
+
+import br.com.redesocial.modelo.dao.EventoDAO;
+import br.com.redesocial.modelo.dto.Evento;
 
 /**
  * Define as regras de negócio para os usuários
  * @author Willian Wallace de Matteus Silva
  * @since 13/10/2017
  */
-public class EventoBO {
+public class EventoBO extends BOCRUDBase<Evento, EventoDAO> {
+  
+    @Override
+    protected EventoDAO instanciarDAO() {
+        return new EventoDAO();
+    }
     
+    /**
+    * Validação da chave primária de um objeto da tabela eventos
+    * @param dto objeto em questão
+    * @throws Exception validação se a chave não foi preenchida
+    */
+    @Override
+    protected void validarChavePrimaria(Evento dto) throws Exception {
+         if (dto.getId() == null) throw new Exception("Preencha o campo id");
+    }
+    
+    /**
+    * Validação dos atributos dos eventos
+    * @param dto objeto em questão que será validado
+    * @throws Exception validações encontradas
+    */
+    @Override
+    protected void validar(Evento dto) throws Exception {
+        
+        if (dto.getNome() == null || dto.getNome().trim().equals("")) throw new Exception("Preencha o nome do evento");
+        
+        if (dto.getDescricao().trim().equals("")) throw new Exception("Preencha a descricao do evento");     
+    }
 }
