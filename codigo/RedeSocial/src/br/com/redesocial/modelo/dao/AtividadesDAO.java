550aef173fe39944ab1e43eb57585a66f52cacdf
Jonathan_Sousa <30672470+Jonathanss123@users.noreply.github.com>
2017-10-25 21:39:43 -0200

    Criar DAO de atividades

--- codigo/RedeSocial/src/br/com/redesocial/modelo/dao/AtividadesDAO.java
+++ codigo/RedeSocial/src/br/com/redesocial/modelo/dao/AtividadesDAO.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.redesocial.modelo.dao;
 
+import br.com.redesocial.modelo.dto.Atividades;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Aluno
  */
-public class Atividades {
+public class AtividadesDAO extends DAOCRUDBase<Atividades> {
     
+    @Override
+    public void alterar(Atividades dto) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Atividades selecionar(int id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List listar() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void excluir(int id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void inserir(Atividades dto) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
