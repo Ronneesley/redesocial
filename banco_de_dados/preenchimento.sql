b0ac006f7a6d40e7240d401be5da5a675bae76a1
Lucianodcb <30538692+Lucianodcb@users.noreply.github.com>
2017-10-29 15:38:00 -0200

    Criação do arquivos sql pra inserçao de dados, para teste

---
+++ banco_de_dados/preenchimento.sql
@@ -0,0 +1,21 @@
+/*Inserindo dados na tabela de paises*/
+INSERT INTO paises 
+(nome) 
+VALUES 
+('Brasil'), 
+('Argentina'),
+('Estados Unidos'),
+('Portugal'),
+('França');
+
+/*Inserindo dados na tabela de estados*/
+INSERT INTO estados
+(nome, pais)
+VALUES
+('Texas', '3'),
+('Minas Gerais', '1'),
+('Santa Fé', '2'),
+('São Paulo', '1'),
+('Flórida', '3');
+
+/*Inserindo dados na tabela de cidades*/
