5770d3e68cb033d22fc27d670ff86d8989d14224
Lucianodcb <30538692+Lucianodcb@users.noreply.github.com>
2017-10-30 00:13:21 -0200

    inserção de dados nas tabelas de cidades e usuarios

--- banco_de_dados/preenchimento.sql
+++ banco_de_dados/preenchimento.sql
@@ -19,3 +19,23 @@ VALUES
 ('Flórida', '3');
 
 /*Inserindo dados na tabela de cidades*/
+INSERT INTO cidades
+(nome, estado)
+VALUES
+('Uruana', '2'),
+('Austin', '1'),
+('Campinas', '4'),
+('Belo Horizonte', '2'),
+('São Paulo', '4');
+
+/*Inserindo dados na tabela de usuarios*/
+INSERT INTO usuarios
+(nome, email, telefone, senha, data_nascimento, sexo, data_cadastro, status, cidade)
+VALUES
+('Luciano Borba', 'ciano@gamil.com', '992934567', '123456789', '1998-12-01', 'M', '2017-09-05 15:38:37', '1', '6'),
+('Lara Caroline', 'lara@gamil.com', '996912347', 'florzinha', '1993-09-03', 'F', '2017-10-09 12:02:39', '1', '2'),
+('Ianka', 'ianka@hotamil.com', '984596076', 'kdjfldjfjj', '1956-04-12', 'F', '2017-08-05 09:31:37', '1', '3'),
+('josé Maria', 'jm@gamil.com', '9749484848', '850380504', '1990-10-03', 'M', '2017-03-09 11:02:39', '1', '5'),
+('Naira Azevedo', 'na@gamil.com', '965456789', 'naiaraazevedo', '1993-03-15', 'F', '2017-02-09 11:02:39', '1', '4');
+
+/*Inserindo dados na tabela de albuns*/
