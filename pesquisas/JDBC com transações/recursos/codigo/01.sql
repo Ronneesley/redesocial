52bf9cb3455e456ce024b9e3ae89b3895afcdfc9
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2017-11-04 17:34:05 -0200

    JDBC no LaTeX

---
+++ pesquisas/JDBC com transações/recursos/codigo/01.sql
@@ -0,0 +1,2 @@
+update clientes set saldo = saldo-200 where id = 1;
+update clientes set saldo = saldo+200 where id = 2;
---
+++ pesquisas/JDBC com transações/recursos/codigo/02.java
@@ -0,0 +1,7 @@
+connection.setAutoCommit(false); 
+// Digite o sql aqui
+
+connection.commit(); 
+// se der errado
+
+connection.rollback(); 
