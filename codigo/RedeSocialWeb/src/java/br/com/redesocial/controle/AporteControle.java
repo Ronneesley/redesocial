fb04cf3bb2fd00e3ec316240d526bd66dbe3920e
Warley01 <30672542+Warley01@users.noreply.github.com>
2017-11-06 16:38:48 -0200

    Revert "Merge branch 'primeira_codificação' of https://github.com/Ronneesley/redesocial into primeira_codificação"
    This reverts commit 76b0df5652a3fae55341f9385d6096029ed8909e, reversing
    changes made to 1b7de7e5afd7b7b5d02c69da72da2faa3cd0497f.

--- codigo/RedeSocialWeb/src/java/br/com/redesocial/controle/AporteControle.java
+++ codigo/RedeSocialWeb/src/java/br/com/redesocial/controle/AporteControle.java
@@ -5,27 +5,24 @@
  */
 package br.com.redesocial.controle;
 
-import br.com.redesocial.modelo.bo.AporteBO;
 import br.com.redesocial.modelo.bo.CategoriaBO;
-import br.com.redesocial.modelo.bo.PostagemBO;
 import br.com.redesocial.modelo.dto.Aporte;
-import br.com.redesocial.modelo.dto.Categoria;
 import br.com.redesocial.modelo.dto.Postagem;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Paulo Henrique
+ * @author Willian Wallace de Matteus Silva
  */
 @WebServlet(name = "AporteControle", urlPatterns = {"/AporteControle"})
-public class AporteControle extends ControleBase {
+public class AporteControle extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -36,10 +33,12 @@ public class AporteControle extends ControleBase {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+       
         String operacao = request.getParameter("operacao");
-
+        
         try {
             switch (operacao){
                 case "CriarNovo":
@@ -59,7 +58,7 @@ public class AporteControle extends ControleBase {
                     break;
             }
         } catch (Exception ex){
-                ex.printStackTrace();
+
         }
     }
 
@@ -102,124 +101,54 @@ public class AporteControle extends ControleBase {
         return "Short description";
     }// </editor-fold>
 
-    private void criarNovo(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        try {
+    private void criarNovo(HttpServletRequest request, HttpServletResponse response) throws Exception{
+       try {
             Aporte aporte = new Aporte();
-
-            request.setAttribute("aporte", aporte);
             
-            CategoriaBO categoriaBO = new CategoriaBO();
-            List categorias = categoriaBO.listar();            
-            request.setAttribute("categorias", categorias);
-        } catch (Exception ex){
+            request.setAttribute("aporte", aporte);
+
+            CategoriaBO  categoriaBO = new CategoriaBO();
+            List categoria = categoriaBO.listar();
+
+            request.setAttribute("categoria", categoria);
+            
+            Postagem postagem = new Postagem();
+            
+            request.setAttribute("postagem", postagem);
+            
+        }
+       catch (Exception ex){
             request.setAttribute("erro", ex.getMessage());
         }
-
-        RequestDispatcher rd = request.getRequestDispatcher("paginas/cadastro_aportes/cadastro_aporte.jsp");
-        rd.forward(request, response);
+       
+       RequestDispatcher rd = request.getRequestDispatcher("paginas/aporte/criar_novo_aporte.jsp");
+       rd.forward(request, response);
     }
 
-    private void cadastrar(HttpServletRequest request, HttpServletResponse response) throws Exception{
+    private void cadastrar(HttpServletRequest request, HttpServletResponse response ) throws Exception {
         Aporte aporte = new Aporte();
         
-        if (!"".equals(request.getParameter("id").trim())){
+        if(!"".equals(request.getParameter("id").trim())){
             aporte.setId(Integer.parseInt(request.getParameter("id")));
         }
         
         aporte.setTitulo(request.getParameter("titulo"));
-
-        int idCategoria = Integer.parseInt( request.getParameter("categoria") );
-        CategoriaBO categoriaBO = new CategoriaBO();
-        Categoria categoria = categoriaBO.selecionar(idCategoria);
-        aporte.setCategoria(categoria);
+        request.setAttribute("titulo", aporte);
         
-        int idPostagem = Integer.parseInt( request.getParameter("postagem") );
-        PostagemBO postagemBO = new PostagemBO();
-        Postagem postagem = postagemBO.selecionar(idPostagem);
-        aporte.setPostagem(postagem);
         
-        request.setAttribute("aporte", aporte);
-        
-        List categorias = categoriaBO.listar();            
-        request.setAttribute("categorias", categorias);
-        
-         if (aporte.getId() == null){
-            this.inserir(aporte, request, response);
-        } else {
-            this.alterar(aporte, request, response);
-        }
 
-        RequestDispatcher rd = request.getRequestDispatcher("paginas/cadastro_aportes/cadastro_aporte.jsp");
-        rd.forward(request, response);
-    }
-    
-        private void inserir(Aporte aporte, HttpServletRequest request, HttpServletResponse response) throws Exception{
-        try {
-            AporteBO aporteBO = new AporteBO();
-            aporteBO.inserir(aporte);
-
-            request.setAttribute("mensagem", "Cadastro realizado com sucesso");
-        } catch (Exception ex){
-            request.setAttribute("erro", ex.getMessage());
-        }
     }
 
-    private void alterar(Aporte aporte, HttpServletRequest request, HttpServletResponse response) throws Exception{
-        try {
-            AporteBO aporteBO = new AporteBO();
-            aporteBO.alterar(aporte);
-
-            request.setAttribute("mensagem", "Alteração realizada com sucesso");
-        } catch (Exception ex){
-            request.setAttribute("erro", ex.getMessage());
-        }
+    private void excluir(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private void listar(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        try {
-            AporteBO bo = new AporteBO();
-            List artigos = bo.listar();
-
-            request.setAttribute("lista", artigos);
-        } catch (Exception ex){
-            request.setAttribute("erro", ex.getMessage());
-        }
-
-        RequestDispatcher rd = request.getRequestDispatcher("paginas/cadastro_aportes/cadastro_aporte.jsp"); //Mudar para a pagina de listagem
-        rd.forward(request, response);
+    private void editar(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private void excluir(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        try {
-            Integer id = Integer.parseInt(request.getParameter("id"));
-
-            AporteBO aporteBO = new AporteBO();
-            aporteBO.excluir(id);
-
-            request.setAttribute("mensagem", "Excluído com sucesso");
-        } catch (Exception ex){
-            request.setAttribute("erro", ex.getMessage());
-        }
-
-        this.listar(request, response); 
-    }
-
-    private void editar(HttpServletRequest request, HttpServletResponse response) throws Exception {
-         try {
-            Integer id = Integer.parseInt(request.getParameter("id"));
-
-            AporteBO aporteBO = new AporteBO();
-            Aporte aporte = aporteBO.selecionar(id);
-
-            request.setAttribute("aporte", aporte);
-
-            request.setAttribute("mensagem", "Registro selecionado com sucesso");
-        } catch (Exception ex){
-            request.setAttribute("erro", ex.getMessage());
-        }
-
-        RequestDispatcher rd = request.getRequestDispatcher("paginas/cadastro_aportes/cadastro_aporte.jsp"); //Mudar para a página de listagem
-        rd.forward(request, response);
+    private void listar(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
